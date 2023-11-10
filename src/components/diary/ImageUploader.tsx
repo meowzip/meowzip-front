61f22652d0fc91baa25b6ff413bd964c250399ca
rectanglehihat <rectanglehihat@gmail.com>
2023-11-10 19:54:50 +0900

    Feat: ImageUploader 수정 및 ImageCropper 파일 삭제

--- src/components/diary/ImageUploader.tsx
+++ src/components/diary/ImageUploader.tsx
@@ -2,14 +2,14 @@
 
 import Image from 'next/image';
 import { ChangeEvent, useRef, useState } from 'react';
-import 'cropperjs/dist/cropper.css';
-import ImageCropper from './ImageCropper';
+import { Button } from '../ui/Button';
+import useCropper from '@/lib/useCropper';
 
 const ImageUploader = () => {
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const selectImage = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
     if (file && file.type.startsWith('image/')) {
       const reader = new FileReader();
@@ -22,13 +22,16 @@ const ImageUploader = () => {
     }
   };
 
-  const handleClick = () => {
-    fileInputRef.current?.click();
-  };
-
   const deleteImage = () => {
     setImageSrc(null);
+    setCroppedImage(null);
   };
+
+  const imageElement = useRef(null);
+  const { handleCrop, croppedImage, setCroppedImage } = useCropper(
+    imageSrc,
+    imageElement
+  );
 
   return (
     <div className="relative flex h-[90px] w-[90px] flex-col items-center justify-center gap-1 rounded-16 bg-gr-50">
@@ -38,18 +41,17 @@ const ImageUploader = () => {
           ref={fileInputRef}
           type="file"
           accept="image/*"
-          onChange={handleImageChange}
+          onChange={selectImage}
           className="hidden"
         />
       )}
       {/* 업로드 버튼 겸 이미지 프리뷰 */}
       <section
-        className="flex h-full w-full items-center justify-center bg-cover bg-center bg-no-repeat text-btn-3 text-gr-300"
-        style={{ backgroundImage: `url(${imageSrc})` }}
-        onClick={handleClick}
+        className="flex h-full w-full items-center justify-center rounded-16 bg-cover bg-center bg-no-repeat text-btn-3 text-gr-300"
+        style={{ backgroundImage: `url(${croppedImage})` }}
+        onClick={() => fileInputRef.current?.click()}
       >
-        {/* 이미지 없을때 */}
-        {!imageSrc && (
+        {!croppedImage && (
           <div className="flex flex-col items-center justify-center gap-1">
             <Image
               src="/images/icons/camera.svg"
@@ -62,23 +64,37 @@ const ImageUploader = () => {
         )}
       </section>
       {/* 삭제 버튼 */}
-      <section
-        className="absolute -right-1 -top-1 cursor-pointer"
-        onClick={deleteImage}
-      >
-        {imageSrc && (
+      {croppedImage && (
+        <section
+          className="absolute -right-1 -top-1 cursor-pointer"
+          onClick={deleteImage}
+        >
           <Image
             src="/images/icons/close-bg.svg"
             alt="icon"
             width={24}
             height={24}
           />
-        )}
-      </section>
+        </section>
+      )}
+      {/* crop image */}
       <section className="w-full">
-        {imageSrc && (
+        {imageSrc && !croppedImage && (
           <div className="fixed top-0 z-[1] ">
-            <ImageCropper imageSrc={imageSrc} />
+            <div className="h-screen w-screen bg-gr-white">
+              <Image
+                ref={imageElement}
+                src={imageSrc}
+                alt="cropped-image"
+                width={24}
+                height={24}
+              />
+              <div className="fixed left-1/2 top-8 -translate-x-1/2">
+                <Button variant="secondary" size="md" onClick={handleCrop}>
+                  이미지 자르기
+                </Button>
+              </div>
+            </div>
           </div>
         )}
       </section>
