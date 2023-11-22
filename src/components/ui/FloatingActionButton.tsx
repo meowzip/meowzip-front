113abcec595182e5d5a9f955ef32617e61f561f0
rectanglehihat <rectanglehihat@gmail.com>
2023-11-22 23:19:30 +0900

    Feat: FloatingActionButton onClick 추가

--- src/components/ui/FloatingActionButton.tsx
+++ src/components/ui/FloatingActionButton.tsx
@@ -2,11 +2,18 @@ import Image from 'next/image';
 
 interface FloatingActionButtonProps {
   image: string;
+  onClick: () => void;
 }
 
-const FloatingActionButton = ({ image }: FloatingActionButtonProps) => {
+const FloatingActionButton = ({
+  image,
+  onClick
+}: FloatingActionButtonProps) => {
   return (
-    <button className="fixed bottom-5 right-5 z-10 flex h-12 w-12 items-center justify-center rounded-full border bg-gr-white">
+    <button
+      className="fixed bottom-5 right-5 z-10 flex h-12 w-12 items-center justify-center rounded-full border bg-gr-white"
+      onClick={onClick}
+    >
       <Image src={image} alt="icon" width={48} height={48} />
     </button>
   );
