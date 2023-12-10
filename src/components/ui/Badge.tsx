71f825dfda0ba49222a2e18287bf3b41b547a21d
rectanglehihat <rectanglehihat@gmail.com>
2023-12-10 22:43:54 +0900

    Design: Badge iconStyle 추가

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -4,20 +4,28 @@ interface BagdeProps {
   icon?: string;
   bgColor?: string;
   textColor?: string;
+  iconStyle?: string;
 }
 
 const badgeTypeVariants = {
   default: 'w-2 h-2 rounded-full',
   text: 'w-5 h-5 text-btn-3 px-2 py-1 rounded-full',
-  icon: 'w-4 h-4 rounded-full'
+  icon: 'rounded-full'
 };
 
-const Badge = ({ type, text, icon, bgColor, textColor }: BagdeProps) => {
+const Badge = ({
+  type,
+  text,
+  icon,
+  bgColor,
+  textColor,
+  iconStyle
+}: BagdeProps) => {
   return (
     <div
       className={`flex items-center justify-center ${bgColor} ${badgeTypeVariants[type]}`}
     >
-      {icon && <img src={icon} alt="label-icon" />}
+      {icon && <img src={icon} alt="label-icon" className={iconStyle} />}
       {text && (
         <p className={`text-btn-3 text-gr-white ${textColor}`}>{text}</p>
       )}
