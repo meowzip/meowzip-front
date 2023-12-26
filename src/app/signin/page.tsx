8749e1ddc78633b99d95b30ba4e0c1c5ec4d68ab
Reese (Yaeji) <notyaeji@gmail.com>
2023-12-26 23:28:17 +0900

    Fix: signin page layout 제거

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -7,32 +7,44 @@ import Password from '@/components/signin/Password';
 import CheckAccount from '@/components/signin/CheckAccount';
 import SignInMain from '@/components/signin/SignInMain';
 import Complete from '@/components/signin/Complete';
+import Topbar from '@/components/ui/Topbar';
 export type NonEmptyArray<T> = readonly [T, ...T[]];
 
-export default function LoginPage() {
+export default function SigninPage() {
   const [Funnel, setStep] = useFunnel(
     ['main', 'email', 'accountInfo', 'password', 'complete'] as const,
     'main'
   );
+
   return (
     <div className="p-[40px 16px 0px 16px] flex-[1 0 0] flex max-w-[640px] flex-col items-center self-stretch">
-      <Funnel>
-        <Funnel.Step name="main">
-          <SignInMain setStep={() => setStep('email')} />
-        </Funnel.Step>
-        <Funnel.Step name="email">
-          <Email setStep={() => setStep('accountInfo')} />
-        </Funnel.Step>
-        <Funnel.Step name="accountInfo">
-          <CheckAccount setStep={() => setStep('complete')} />
-        </Funnel.Step>
-        <Funnel.Step name="password">
-          <Password setStep={() => setStep('complete')} />
-        </Funnel.Step>
-        <Funnel.Step name="complete">
-          <Complete />
-        </Funnel.Step>
-      </Funnel>
+      <div className="funnel-container mt-16 w-full">
+        <Funnel>
+          <Funnel.Step name="main">
+            <SignInMain setStep={() => setStep('email')} />
+          </Funnel.Step>
+          <Funnel.Step name="email">
+            <Email setStep={() => setStep('accountInfo')} />
+          </Funnel.Step>
+          <Funnel.Step name="accountInfo">
+            <div className="fixed top-0 w-full">
+              <Topbar
+                hideRight
+                type="modal"
+                title="계정 확인"
+                onClick={() => history.go(-1)}
+              />
+            </div>
+            <CheckAccount setStep={() => setStep('complete')} />
+          </Funnel.Step>
+          <Funnel.Step name="password">
+            <Password setStep={() => setStep('complete')} />
+          </Funnel.Step>
+          <Funnel.Step name="complete">
+            <Complete />
+          </Funnel.Step>
+        </Funnel>
+      </div>
     </div>
   );
 }
