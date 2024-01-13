86528ea2c197bc5c60296c002f56ace15e987975
rectanglehihat <rectanglehihat@gmail.com>
2024-01-13 17:49:02 +0900

    Feat: 회원 정보 수정 api 연결 중

--- src/services/nickname.ts
+++ src/services/nickname.ts
@@ -1,7 +1,7 @@
 import returnFetchJson from '@/utils/returnFetchJson';
 
 const token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MjcsImlhdCI6MTcwNDgxMDQ1NCwiZXhwIjoxNzA0ODE0MDU0fQ.-eiPmP1YaKn9xNzpo5AauZeESDY8w1lstlXAgGLDuSI';
+  'eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MjcsImlhdCI6MTcwNTEzMTA0OCwiZXhwIjoxNzA1MTM0NjQ4fQ.flP5mYbjxpBvK9mTjaWF8ct4bpSMkdXCCITK6I4sPfQ';
 
 export const fetchExtendedAuth = returnFetchJson({
   baseUrl: process.env.NEXT_PUBLIC_AUTH_MEOW_API,
@@ -21,6 +21,29 @@ export const validateNicknameOnServer = async (nickname: string) => {
       throw new Error('닉네임 유효성 검사 중 오류 발생:' + error.message);
     } else {
       throw new Error('닉네임 유효성 검사 중 오류 발생:');
+    }
+  }
+};
+
+export const updateProfileOnServer = async (reqObj: {
+  nickname: string;
+  profileImage: string | null;
+}) => {
+  try {
+    const requestOptions = {
+      method: 'PATCH',
+      body: reqObj
+    };
+
+    const response = await fetchExtendedAuth('/members', requestOptions);
+
+    return response.body;
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      throw new Error('프로필 수정 중 오류 발생:' + error.message);
+    } else {
+      throw new Error('프로필 수정 중 오류 발생:');
     }
   }
 };
