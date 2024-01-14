993fa595aa02720a4cfbef57959f80690cb9bb19
rectanglehihat <rectanglehihat@gmail.com>
2024-01-14 16:58:02 +0900

    Feat: 비밀번호 재설정 API 연결

--- src/app/reset-pwd/page.tsx
+++ src/app/reset-pwd/page.tsx
@@ -3,16 +3,48 @@
 import { Input } from '../../components/ui/Input';
 import { Button } from '../../components/ui/Button';
 import { resetPwdOnServer } from '../../services/signup';
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useMutation } from '@tanstack/react-query';
 import usePasswordHandler from '../../utils/usePasswordHandler';
+import Modal from '@/components/ui/Modal';
 
 const ResetPwdPage = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [token, setToken] = useState('');
+  const [openModal, setOpenModal] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
+
   const { password, passwordCheck, handlePwdChange, handlePwdCheckChange } =
     usePasswordHandler();
 
+  useEffect(() => {
+    const token = searchParams.get('token');
+    if (!token) return;
+
+    setToken(token);
+  }, []);
+
+  /**
+   * @description API - POST reset password
+   */
+  const signUpMutation = useMutation({
+    mutationFn: (reqObj: { password: string; token: string }) =>
+      resetPwdOnServer(reqObj),
+    onSuccess: (data: any) => {
+      if (data.status !== 'OK') {
+        data.message && setErrorMsg(data.message);
+        setOpenModal(true);
+      } else {
+        router.push('/signin');
+      }
+    }
+  });
+
   const resetPwd = () => {
-    const data = resetPwdOnServer({ password: password.value, token: 'asdf' });
-    console.log('data', data);
-    // router.push('/signin');
+    signUpMutation.mutate({ password: password.value, token: token });
   };
 
   return (
@@ -57,6 +89,23 @@ const ResetPwdPage = () => {
           재설정하기
         </Button>
       </article>
+      {openModal && (
+        <Modal
+          contents={{ title: '알림', body: errorMsg }}
+          scrim={true}
+          buttons={[
+            {
+              variant: 'primary',
+              size: 'lg',
+              content: '확인',
+              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700'
+            }
+          ]}
+          onClose={() => {
+            setOpenModal(false);
+          }}
+        />
+      )}
     </section>
   );
 };
