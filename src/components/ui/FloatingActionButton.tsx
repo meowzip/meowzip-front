690af2f2ccd55fb65b38141a654299ca97ee7f46
rectanglehihat <rectanglehihat@gmail.com>
2024-01-28 19:54:51 +0900

    Design: FloatingActionButton z-index 수정

--- src/components/ui/FloatingActionButton.tsx
+++ src/components/ui/FloatingActionButton.tsx
@@ -7,7 +7,7 @@ interface FloatingActionButtonProps {
 const FloatingActionButton = ({ onClick }: FloatingActionButtonProps) => {
   return (
     <button
-      className="fixed bottom-[100px] right-5 z-[999] flex h-12 w-12 items-center justify-center rounded-full bg-pr-500"
+      className="fixed bottom-[100px] right-5 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-pr-500"
       onClick={onClick}
     >
       <Image src="/images/icons/plus.svg" alt="icon" width={24} height={24} />
