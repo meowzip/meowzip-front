31eb996ac6a7ec6d5c48c537bdb78869bb525b4a
rectanglehihat <rectanglehihat@gmail.com>
2024-02-15 21:52:57 +0900

    Feat: 피드 더보기 버튼 기능 구현

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -6,11 +6,16 @@ import FloatingActionButton from '@/components/ui/FloatingActionButton';
 import FeedWriteModal from '@/components/community/FeedWriteModal';
 import BottomSheet from '@/components/ui/BottomSheet';
 import ActionButton from '@/components/ui/ActionButton';
+import Modal from '@/components/ui/Modal';
 
 const CommunityPage = () => {
-  const [showWriteModal, setShowWriteModal] = useState(false);
   const [editBottomSheet, setEditBottomSheet] = useState(false);
-  const [isMyFeed, setIsMyFeed] = useState(true);
+  const [showWriteModal, setShowWriteModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showReportModal, setShowReportModal] = useState(false);
+  const [showBlockModal, setShowBlockModal] = useState(false);
+  const [name, setName] = useState('이치즈');
+  const [isMyFeed, setIsMyFeed] = useState(false);
   const [feedList, setFeedList] = useState([
     {
       id: 1,
@@ -42,6 +47,38 @@ const CommunityPage = () => {
     }
   ]);
 
+  const openModalEdit = () => {
+    setShowWriteModal(true);
+    setEditBottomSheet(false);
+  };
+  const openModalDelete = () => {
+    setShowEditModal(true);
+    setEditBottomSheet(false);
+  };
+  const openModalReport = () => {
+    setShowReportModal(true);
+    setEditBottomSheet(false);
+    console.log('open Modal Report');
+  };
+  const openModalBlock = () => {
+    setShowBlockModal(true);
+    setEditBottomSheet(false);
+    console.log('open Modal Block');
+  };
+
+  const deleteFeed = () => {
+    console.log('삭제 버튼 클릭');
+    setShowEditModal(false);
+  };
+  const reportFeed = () => {
+    console.log('신고 버튼 클릭');
+    setShowReportModal(false);
+  };
+  const blockFeed = () => {
+    console.log('차단 버튼 클릭');
+    setShowBlockModal(false);
+  };
+
   return (
     <>
       <h1 className="flex h-12 items-center pl-4 align-middle text-heading-3 text-gr-900">
@@ -70,12 +107,12 @@ const CommunityPage = () => {
             <ActionButton
               icon="/images/icons/edit.svg"
               content="수정하기"
-              onClick={() => console.log('open modal:EDIT')}
+              onClick={() => openModalEdit()}
             />
             <ActionButton
               icon="/images/icons/delete.svg"
               content="삭제하기"
-              onClick={() => console.log('open modal:DELETE')}
+              onClick={() => openModalDelete()}
             />
           </>
         ) : (
@@ -83,16 +120,85 @@ const CommunityPage = () => {
             <ActionButton
               icon="/images/icons/edit.svg"
               content="게시물 신고하기"
-              onClick={() => console.log('open modal:REPORT')}
+              onClick={() => openModalReport()}
             />
             <ActionButton
               icon="/images/icons/delete.svg"
               content="작성자 차단하기"
-              onClick={() => console.log('open modal:BLOCK')}
+              onClick={() => openModalBlock()}
             />
           </>
         )}
       </BottomSheet>
+
+      {showEditModal && (
+        <Modal
+          contents={{ title: '삭제하시겠습니까?' }}
+          scrim={true}
+          buttons={[
+            {
+              variant: 'primary',
+              size: 'lg',
+              content: '삭제',
+              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700',
+              onClick: () => deleteFeed()
+            },
+            {
+              variant: 'text',
+              size: 'lg',
+              content: '나중에 할게요',
+              style: 'w-full rounded-[16px] px-4 py-2 text-gr-300',
+              onClick: () => setShowEditModal(false)
+            }
+          ]}
+        />
+      )}
+
+      {showReportModal && (
+        <Modal
+          contents={{ title: '해당 게시글을 \n 신고하시겠습니까?' }}
+          scrim={true}
+          buttons={[
+            {
+              variant: 'primary',
+              size: 'lg',
+              content: '신고하기',
+              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700',
+              onClick: () => reportFeed()
+            },
+            {
+              variant: 'text',
+              size: 'lg',
+              content: '나중에 할게요',
+              style: 'w-full rounded-[16px] px-4 py-2 text-gr-300',
+              onClick: () => setShowReportModal(false)
+            }
+          ]}
+        />
+      )}
+
+      {showBlockModal && (
+        <Modal
+          contents={{ title: `${name}님을 \n 차단하시겠습니까?` }}
+          scrim={true}
+          buttons={[
+            {
+              variant: 'primary',
+              size: 'lg',
+              content: '신고하기',
+              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700',
+              onClick: () => blockFeed()
+            },
+            {
+              variant: 'text',
+              size: 'lg',
+              content: '나중에 할게요',
+              style: 'w-full rounded-[16px] px-4 py-2 text-gr-300',
+              onClick: () => setShowBlockModal(false)
+            }
+          ]}
+        />
+      )}
     </>
   );
 };
