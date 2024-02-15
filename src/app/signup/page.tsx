8040df61db4b2d39cf5e4351f871c7d8a88b0df5
rectanglehihat <rectanglehihat@gmail.com>
2024-02-15 21:51:49 +0900

    Feat: Modal onClose props 삭제, button props에 onClick 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -128,12 +128,12 @@ const SignUpPage = () => {
               variant: 'primary',
               size: 'lg',
               content: '확인',
-              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700'
+              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700',
+              onClick: () => {
+                setOpenModal(false), setOpenAgreeBottom(false);
+              }
             }
           ]}
-          onClose={() => {
-            setOpenModal(false), setOpenAgreeBottom(false);
-          }}
         />
       )}
     </section>
