377d0b32fcb25d85d7ac8b3039b52cac33a4c7e8
rectanglehihat <rectanglehihat@gmail.com>
2024-02-25 18:26:33 +0900

    Feat: 커뮤니티 컴포넌트 분리 (MoreBtnBottomSheet 컴포넌트 생성)

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -4,21 +4,14 @@ import React, { useState } from 'react';
 import FeedCard from '../../components/community/FeedCard';
 import FloatingActionButton from '@/components/ui/FloatingActionButton';
 import FeedWriteModal from '@/components/community/FeedWriteModal';
-import BottomSheet from '@/components/ui/BottomSheet';
-import ActionButton from '@/components/ui/ActionButton';
-import Modal from '@/components/ui/Modal';
+import MoreBtnBottomSheet from '@/components/community/MoreBtnBottomSheet';
+import { useAtom } from 'jotai';
+import { showWriteModalAtom } from '@/atoms/modalAtom';
 
 const CommunityPage = () => {
   const [editBottomSheet, setEditBottomSheet] = useState(false);
-  const [showWriteModal, setShowWriteModal] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState<{
-    title: string;
-    body?: string;
-    primaryBtn: { content: string; onClick: () => void };
-  }>();
+  const [showWriteModal, setShowWriteModal] = useAtom(showWriteModalAtom);
   const [name, setName] = useState('이치즈');
-  const [isMyFeed, setIsMyFeed] = useState(true);
   const [feedList, setFeedList] = useState([
     {
       id: 1,
@@ -50,49 +43,6 @@ const CommunityPage = () => {
     }
   ]);
 
-  const openModalEdit = () => {
-    setEditBottomSheet(false);
-    setShowWriteModal(true);
-  };
-  const openModalDelete = () => {
-    setShowModal(true);
-    setEditBottomSheet(false);
-    setModalContent({
-      title: '삭제하시겠습니까?',
-      primaryBtn: { content: '삭제', onClick: () => deleteFeed() }
-    });
-  };
-  const openModalReport = () => {
-    setEditBottomSheet(false);
-    setShowModal(true);
-    setModalContent({
-      title: '해당 게시글을 \n 신고하시겠습니까?',
-      primaryBtn: { content: '신고하기', onClick: () => reportFeed() }
-    });
-  };
-  const openModalBlock = () => {
-    setEditBottomSheet(false);
-    setShowModal(true);
-    setModalContent({
-      title: `${name}님을 \n 차단하시겠습니까?`,
-      body: '차단된 사용자의 게시글과 댓글을 회원님께 \n 더이상 표시하지 않습니다.',
-      primaryBtn: { content: '차단하기', onClick: () => blockFeed() }
-    });
-  };
-
-  const deleteFeed = () => {
-    console.log('삭제 버튼 클릭');
-    setShowModal(false);
-  };
-  const reportFeed = () => {
-    console.log('신고 버튼 클릭');
-    setShowModal(false);
-  };
-  const blockFeed = () => {
-    console.log('차단 버튼 클릭');
-    setShowModal(false);
-  };
-
   return (
     <>
       <h1 className="flex h-12 items-center pl-4 align-middle text-heading-3 text-gr-900">
@@ -105,71 +55,16 @@ const CommunityPage = () => {
           onClick={() => setEditBottomSheet(true)}
         />
       ))}
-      <FloatingActionButton onClick={() => setShowWriteModal(true)} />
-
       {showWriteModal && (
         <FeedWriteModal onClose={() => setShowWriteModal(false)} />
       )}
-
-      <BottomSheet
+      <FloatingActionButton onClick={() => setShowWriteModal(true)} />
+      <MoreBtnBottomSheet
         isVisible={editBottomSheet}
         setIsVisible={() => setEditBottomSheet(!editBottomSheet)}
-        topBar={true}
         heightPercent={['40%', '40%']}
-      >
-        <div className="px-4">
-          {isMyFeed ? (
-            <>
-              <ActionButton
-                icon="/images/icons/edit.svg"
-                content="수정하기"
-                onClick={() => openModalEdit()}
-              />
-              <ActionButton
-                icon="/images/icons/delete.svg"
-                content="삭제하기"
-                onClick={() => openModalDelete()}
-              />
-            </>
-          ) : (
-            <>
-              <ActionButton
-                icon="/images/icons/edit.svg"
-                content="게시물 신고하기"
-                onClick={() => openModalReport()}
-              />
-              <ActionButton
-                icon="/images/icons/delete.svg"
-                content="작성자 차단하기"
-                onClick={() => openModalBlock()}
-              />
-            </>
-          )}
-        </div>
-      </BottomSheet>
-
-      {showModal && (
-        <Modal
-          contents={{ title: modalContent?.title, body: modalContent?.body }}
-          scrim={true}
-          buttons={[
-            {
-              variant: 'primary',
-              size: 'lg',
-              content: modalContent?.primaryBtn.content || '',
-              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700',
-              onClick: modalContent?.primaryBtn.onClick
-            },
-            {
-              variant: 'text',
-              size: 'lg',
-              content: '나중에 할게요',
-              style: 'w-full rounded-[16px] px-4 py-2 text-gr-300',
-              onClick: () => setShowModal(false)
-            }
-          ]}
-        />
-      )}
+        name={name}
+      />
     </>
   );
 };
---
+++ src/components/community/MoreBtnBottomSheet.tsx
@@ -0,0 +1,138 @@
+import { showWriteModalAtom } from '@/atoms/modalAtom';
+import ActionButton from '@/components/ui/ActionButton';
+import BottomSheet from '@/components/ui/BottomSheet';
+import Modal from '@/components/ui/Modal';
+import { useAtom } from 'jotai';
+import React, { useState } from 'react';
+
+interface MoreBtnBottomSheetProps {
+  isVisible: boolean;
+  setIsVisible: (isVisible: boolean) => void;
+  heightPercent: string[];
+  name: string;
+}
+
+const MoreBtnBottomSheet: React.FC<MoreBtnBottomSheetProps> = ({
+  isVisible,
+  setIsVisible,
+  heightPercent,
+  name
+}) => {
+  const [showWriteModal, setShowWriteModal] = useAtom(showWriteModalAtom);
+  const [isMyFeed, setIsMyFeed] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState<{
+    title: string;
+    body?: string;
+    primaryBtn: { content: string; onClick: () => void };
+  }>();
+
+  const openModalEdit = () => {
+    setIsVisible(false);
+    setShowWriteModal(true);
+  };
+  const openModalDelete = () => {
+    setIsVisible(false);
+    setShowModal(true);
+    setModalContent({
+      title: '삭제하시겠습니까?',
+      primaryBtn: { content: '삭제', onClick: () => deleteFeed() }
+    });
+  };
+  const openModalReport = () => {
+    setIsVisible(false);
+    setShowModal(true);
+    setModalContent({
+      title: '해당 게시글을 \n 신고하시겠습니까?',
+      primaryBtn: { content: '신고하기', onClick: () => reportFeed() }
+    });
+  };
+  const openModalBlock = () => {
+    setIsVisible(false);
+    setShowModal(true);
+    setModalContent({
+      title: `${name}님을 \n 차단하시겠습니까?`,
+      body: '차단된 사용자의 게시글과 댓글을 회원님께 \n 더이상 표시하지 않습니다.',
+      primaryBtn: { content: '차단하기', onClick: () => blockFeed() }
+    });
+  };
+
+  const deleteFeed = () => {
+    console.log('삭제 버튼 클릭');
+    setShowModal(false);
+  };
+  const reportFeed = () => {
+    console.log('신고 버튼 클릭');
+    setShowModal(false);
+  };
+  const blockFeed = () => {
+    console.log('차단 버튼 클릭');
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <BottomSheet
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+        topBar={true}
+        heightPercent={heightPercent}
+      >
+        <div className="px-4">
+          {isMyFeed ? (
+            <>
+              <ActionButton
+                icon="/images/icons/edit.svg"
+                content="수정하기"
+                onClick={() => openModalEdit()}
+              />
+              <ActionButton
+                icon="/images/icons/delete.svg"
+                content="삭제하기"
+                onClick={() => openModalDelete()}
+              />
+            </>
+          ) : (
+            <>
+              <ActionButton
+                icon="/images/icons/edit.svg"
+                content="게시물 신고하기"
+                onClick={() => openModalReport()}
+              />
+              <ActionButton
+                icon="/images/icons/delete.svg"
+                content="작성자 차단하기"
+                onClick={() => openModalBlock()}
+              />
+            </>
+          )}
+        </div>
+      </BottomSheet>
+
+      {showModal && (
+        <Modal
+          contents={{ title: modalContent?.title, body: modalContent?.body }}
+          scrim={true}
+          buttons={[
+            {
+              variant: 'primary',
+              size: 'lg',
+              content: modalContent?.primaryBtn.content || '',
+              style: 'w-full rounded-[16px] px-4 py-2 bg-sm-error-700',
+              onClick: modalContent?.primaryBtn.onClick
+            },
+            {
+              variant: 'text',
+              size: 'lg',
+              content: '나중에 할게요',
+              style: 'w-full rounded-[16px] px-4 py-2 text-gr-300',
+              onClick: () => setShowModal(false)
+            }
+          ]}
+        />
+      )}
+    </>
+  );
+};
+
+export default MoreBtnBottomSheet;
