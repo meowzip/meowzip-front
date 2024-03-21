37a2c6d3d2e91504c174535f4febd19599ceeecd
Reese (Yaeji) <notyaeji@gmail.com>
2024-03-22 00:07:34 +0900

    Feat: middleware에 token refresh 기능 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,70 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import returnFetchJson from '@/utils/returnFetchJson';
+
+const fetchExtended = returnFetchJson({
+  baseUrl: process.env.NEXT_PUBLIC_MEOW_API,
+  headers: { Accept: 'application/json' }
+});
 
 const protectedRoutes = ['/diary'];
 const publicRoutes = ['/signin', '/signup'];
 
-export function middleware(request: NextRequest) {
-  const token = getTokenFromCookies(request);
+export const middleware = async (request: NextRequest) => {
   const currentPath = request.nextUrl.pathname;
 
-  if (!token && protectedRoutes.includes(currentPath)) {
+  const accessToken = await getTokenFromCookies(request);
+  console.log('accessToken:', accessToken);
+  if (accessToken && checkExpiredToken(accessToken)) {
+    try {
+      await refreshAccessToken();
+    } catch (error) {
+      console.error('Error:', error);
+      const url = request.nextUrl.clone();
+      url.pathname = '/signin';
+      return NextResponse.redirect(url);
+    }
+  }
+
+  if (!accessToken && protectedRoutes.includes(currentPath)) {
     const url = request.nextUrl.clone();
     url.pathname = '/signin';
     return NextResponse.redirect(url);
   }
 
-  if (token && publicRoutes.includes(currentPath)) {
+  if (accessToken && publicRoutes.includes(currentPath)) {
     const url = request.nextUrl.clone();
     url.pathname = '/';
     return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
-}
+};
 
-function getTokenFromCookies(request: NextRequest) {
+const checkExpiredToken = (accessToken: string) => {
+  const decodedToken = JSON.parse(atob(accessToken.split('.')[1]));
+  const exp = decodedToken.exp * 1000;
+  const now = Date.now();
+  return exp < now;
+};
+
+const refreshAccessToken = async () => {
+  const response = await fetchExtended('/tokens/refresh', { method: 'POST' });
+
+  if (!response.ok) {
+    throw new Error('Failed to refresh token');
+  }
+
+  const token = response.headers.get('Authorization');
+
+  if (token) {
+    document.cookie = `Authorization=${token}; path=/; max-age=3600; secure;`;
+  } else {
+    throw new Error('Authorization token not found in the response');
+  }
+};
+
+const getTokenFromCookies = async (request: NextRequest) => {
   const cookiesHeader = request.headers.get('cookie');
   if (!cookiesHeader) return null;
 
@@ -35,5 +76,6 @@ function getTokenFromCookies(request: NextRequest) {
     });
 
   const cookies = new Map(cookiesArray);
+
   return cookies.get('Authorization');
-}
+};
