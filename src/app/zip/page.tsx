3a24512e92399e95c8204dfcd842736b7aad254b
rectanglehihat <rectanglehihat@gmail.com>
2024-04-06 16:37:56 +0900

    Fix: ZipCard label 수정

--- src/app/zip/page.tsx
+++ src/app/zip/page.tsx
@@ -6,9 +6,14 @@ export default function ZipPage() {
       <h1 className="flex h-12 items-center px-4 text-heading-3 text-gr-900">
         모음집
       </h1>
-      <div className="h-screen bg-gr-50 p-4">
-        <ZipCard />
-      </div>
+      <section className="h-screen bg-gr-50 p-4">
+        <div className="grid grid-cols-2 gap-4 ">
+          <ZipCard />
+          <ZipCard />
+          <ZipCard />
+          <ZipCard />
+        </div>
+      </section>
     </>
   );
 }
