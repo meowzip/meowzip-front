5963a149120287f0b77e711b3305f899b4a339a4
rectanglehihat <rectanglehihat@gmail.com>
2024-04-06 17:33:11 +0900

    Feat: Add catType, 고양이 조회 api 연결

--- src/app/zip/page.tsx
+++ src/app/zip/page.tsx
@@ -1,6 +1,27 @@
-import ZipCard from '@/components/zip/ZipCard';
+'use client';
 
-export default function ZipPage() {
+import { catsAtom } from '@/atoms/catsAtom';
+import ZipCard from '@/components/zip/ZipCard';
+import { getCatsOnServer } from '@/services/cat';
+import { useQuery } from '@tanstack/react-query';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+
+const ZipPage = () => {
+  const [cats, setCats] = useAtom(catsAtom);
+
+  const { data: catData } = useQuery({
+    queryKey: ['getCats'],
+    queryFn: () => getCatsOnServer({ page: 0, size: 10 }),
+    staleTime: 1000 * 60 * 10
+  });
+
+  useEffect(() => {
+    if (catData) {
+      setCats(catData);
+    }
+  }, [catData]);
+
   return (
     <>
       <h1 className="flex h-12 items-center px-4 text-heading-3 text-gr-900">
@@ -8,12 +29,13 @@ export default function ZipPage() {
       </h1>
       <section className="h-screen bg-gr-50 p-4">
         <div className="grid grid-cols-2 gap-4 ">
-          <ZipCard />
-          <ZipCard />
-          <ZipCard />
-          <ZipCard />
+          {cats.map(cat => (
+            <ZipCard key={cat.id} {...cat} />
+          ))}
         </div>
       </section>
     </>
   );
-}
+};
+
+export default ZipPage;
--- src/components/zip/ZipCard.tsx
+++ src/components/zip/ZipCard.tsx
@@ -1,35 +1,54 @@
 'use client';
 
+import { ZipCardProps } from '@/app/zip/catType';
 import Label from '@/components/ui/Label';
 import React from 'react';
 
-const ZipCard = () => {
+const ZipCard = ({
+  imageUrl,
+  name,
+  isCoParented,
+  coParentedCount,
+  dDay,
+  sex,
+  isNeutered
+}: ZipCardProps) => {
   return (
     <div className="relative">
       <div className="absolute right-0 top-0 flex items-center gap-1 p-[5px]">
-        <Label.Text
-          className="bg-gr-transparent-white text-gr-800"
-          content="TNR"
-        />
-        <Label.Default
-          className="bg-gr-transparent-black text-gr-white"
-          src="/images/icons/share.svg"
-          content="2"
-        />
+        {isNeutered && (
+          <Label.Text
+            className="bg-gr-transparent-white text-gr-800"
+            content="TNR"
+          />
+        )}
+        {isCoParented && (
+          <Label.Default
+            className="bg-gr-transparent-black text-gr-white"
+            src="/images/icons/share.svg"
+            content={coParentedCount.toString()}
+          />
+        )}
       </div>
       <img
-        src="https://i.ytimg.com/vi/YCaGYUIfdy4/maxresdefault.jpg"
+        src={imageUrl}
         alt="cat-image"
         className="h-[200px] rounded-t-lg object-cover"
       />
       <div className="rounded-b-lg bg-gr-white px-4 pb-4 pt-3">
         <div className="flex gap-1 pb-[6px]">
-          <p className="text-heading-5 text-gr-900">name</p>
+          <p className="text-heading-5 text-gr-900">{name}</p>
           <div className="rounded-full bg-[#FFF2F1]">
-            <img src="/images/icons/gender-female.svg" alt="gender" />
+            <img
+              src={`/images/icons/gender-${sex}.svg`}
+              alt="tag cat"
+              className={`rounded-full ${
+                sex === 'F' ? 'bg-[#FFF2F1]' : 'bg-[#ECF5FF]'
+              }`}
+            />
           </div>
         </div>
-        <p className="text-btn-3 text-gr-600">만난지 100일</p>
+        <p className="text-btn-3 text-gr-600">만난지 {dDay}일</p>
       </div>
     </div>
   );
