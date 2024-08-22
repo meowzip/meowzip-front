864a9fcc4aed835bc0d22bb4bd85663661e74c89
Reese (Yaeji) <notyaeji@gmail.com>
2024-08-22 23:48:32 +0900

    Feat: 프로필 메인 페이지 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,46 @@
+'use client';
+import { profileImageAtom } from '@/atoms/imageAtom';
+import { useAtom } from 'jotai';
+import OnboardProfileUploader from '@/components/onboard/OnboardProfileUploader';
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger
+} from '@/components/ui/TabsWithLine';
+import Detail from '@/components/profile/Detail';
+
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const [profileImage, setProfileImage] = useAtom(profileImageAtom);
+
+  console.log('profileImage', profileImage);
+
+  return (
+    <>
+      <div className="flex h-12 w-full items-center justify-between bg-gr-white px-4 align-middle text-heading-3 text-gr-900">
+        <h1>프로필</h1>
+        <div>
+          <button>버튼1</button>
+          <button>버튼2</button>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <Tabs defaultValue="account" className="w-[400px]">
+          <OnboardProfileUploader data={profileImage} />
+          <Detail />
+          <TabsList>
+            <TabsTrigger value="myContents">작성한 글</TabsTrigger>
+            <TabsTrigger value="savedContents">저장한 글</TabsTrigger>
+          </TabsList>
+          <TabsContent value="myContents">
+            작성한 글 리스트 컴포넌트
+          </TabsContent>
+          <TabsContent value="savedContents">
+            저장한 글 리스트 컴포넌트
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 }
