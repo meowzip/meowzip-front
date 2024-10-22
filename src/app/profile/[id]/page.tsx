a7a75e9119c91d80333440f2efef2c5846f77852
rectanglehihat <rectanglehihat@gmail.com>
2024-10-22 22:33:30 +0900

    Feat: 다른 유저의 프로필 페이지 개발 중

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,12 @@
 'use client';
-import Image from 'next/image';
 import { profileImageAtom } from '@/atoms/imageAtom';
 import { useAtom } from 'jotai';
-import OnboardProfileUploader from '@/components/onboard/OnboardProfileUploader';
 import { useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import Detail from '@/components/profile/ProfileDetail';
-import { getClickedUserProfile, getMyProfile } from '@/services/profile';
-import { useEffect } from 'react';
+import { getClickedUserProfile } from '@/services/profile';
+import Profile from '@/components/ui/Profile';
+import Topbar from '@/components/ui/Topbar';
 
 const ProfileIdPage = ({ params: { id } }: { params: { id: number } }) => {
   const [profileImage, setProfileImage] = useAtom(profileImageAtom);
@@ -23,38 +22,52 @@ const ProfileIdPage = ({ params: { id } }: { params: { id: number } }) => {
     queryFn: () => getClickedUserProfile(id)
   });
 
+  const goBackTo = () => {
+    console.log('go back to community');
+  };
+
   console.log(othersProfile, 'othersProfile');
 
   return (
     <>
-      <div className="flex h-12 w-full items-center justify-between bg-gr-white px-4 align-middle text-heading-3 text-gr-900">
-        <h1>프로필</h1>
-        <div className="flex gap-3">
-          <button onClick={() => router.push('/profile/alarm')}>
-            <Image
-              src="https://meowzip.s3.ap-northeast-2.amazonaws.com/images/icon/profile/alert.svg"
-              alt="edit"
-              width={24}
-              height={24}
-            />
-          </button>
-          <button onClick={() => router.push('/profile/setting')}>
-            <Image
-              src="https://meowzip.s3.ap-northeast-2.amazonaws.com/images/icon/profile/setting.svg"
-              alt="edit"
-              width={24}
-              height={24}
-            />
-          </button>
-        </div>
-      </div>
-      <div className="flex gap-2">
-        <OnboardProfileUploader
-          data={profileImage}
-          setProfileImage={setProfileImage}
+      <section className="h-12">
+        <Topbar type="three">
+          <Topbar.Back onClick={goBackTo} />
+          <Topbar.Title title={othersProfile?.nickname} />
+          <Topbar.Empty />
+        </Topbar>
+      </section>
+      <section className="flex justify-center py-4">
+        <Profile
+          items={[
+            {
+              id: id,
+              imageUrl: othersProfile?.profileImageUrl,
+              style: 'w-[72px] h-[72px]'
+            }
+          ]}
+          lastLeft="left-[100px]"
         />
-        <Detail />
-        {/* {myFeedList?.map((feed: FeedType) => (
+      </section>
+      {/* <Detail /> */}
+      <section className="flex items-center justify-center self-stretch px-6">
+        <div
+          className="flex max-w-[100px] flex-1 flex-col items-center justify-center px-4 py-3"
+          aria-label="Cat Count"
+        >
+          <div className="font-bold">20</div>
+          <div className="text-gr-600">고양이</div>
+        </div>
+        <div className="h-5 w-[1px] bg-gr-100" aria-hidden="true"></div>
+        <div
+          className="flex max-w-[100px] flex-1 flex-col items-center justify-center px-4 py-3"
+          aria-label="Post Count"
+        >
+          <div className="font-bold">100</div>
+          <div className="text-gr-600">게시물</div>
+        </div>
+      </section>
+      {/* {myFeedList?.map((feed: FeedType) => (
               <FeedCard
                 likeFeed={() => {}}
                 unLikeFeed={() => {}}
@@ -65,7 +78,6 @@ const ProfileIdPage = ({ params: { id } }: { params: { id: number } }) => {
                 goToDetail={() => router.push(`/community/${feed.id}`)}
               />
             ))} */}
-      </div>
     </>
   );
 };
