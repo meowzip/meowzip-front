aa72f0859aaccb3638bd212729a0ecd6aef309aa
rectanglehihat <rectanglehihat@gmail.com>
2024-10-23 17:41:13 +0900

    Style: 모음집 layout padding-bottom 수정

--- src/app/zip/layout.tsx
+++ src/app/zip/layout.tsx
@@ -2,7 +2,7 @@ import BottomNavBar from '@/components/ui/BottomNavBar';
 
 export default function ZipLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section>
+    <section className="h-screen bg-gr-50">
       {children}
       <div className="fixed bottom-0 left-0 right-0 z-10 flex justify-center">
         <BottomNavBar />
