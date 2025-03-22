b124d358e19be3187a485a2a9129f87f9c1e4c67
rectanglehihat <rectanglehihat@gmail.com>
2025-03-22 22:56:42 +0900

    Fix: FloatingActionButton z-index and bottom position

--- src/components/ui/FloatingActionButton.tsx
+++ src/components/ui/FloatingActionButton.tsx
@@ -6,7 +6,7 @@ interface FloatingActionButtonProps {
 
 const FloatingActionButton = ({ onClick }: FloatingActionButtonProps) => {
   return (
-    <div className="fixed bottom-[100px] left-1/2 z-10 z-20 w-full max-w-[640px] -translate-x-1/2 transform">
+    <div className="fixed bottom-[160px] left-1/2 z-10 w-full max-w-[640px] -translate-x-1/2 transform">
       <button
         className="absolute right-5 flex h-12 w-12 items-center justify-center rounded-full bg-pr-500"
         onClick={onClick}
