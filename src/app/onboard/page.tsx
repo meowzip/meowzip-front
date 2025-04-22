2347e3eb5bd302aa855e020c2965c2867d79e123
rectanglehihat <rectanglehihat@gmail.com>
2025-04-23 00:36:51 +0900

    Fix: 회원가입 후 온보딩 페이지로 이동

--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -27,7 +27,7 @@ const OnBoardPage = () => {
   if (isError) throw error;
 
   return (
-    <section className="mx-auto max-w-[640px] px-4 pt-[60px]">
+    <section className="mx-auto h-full max-w-[640px] bg-gr-white px-4 pt-[60px]">
       <article className="flex items-center justify-center">
         <Image
           src={myProfile?.profileImageUrl || DEFAULT_PROFILE_IMAGE_SRC}
