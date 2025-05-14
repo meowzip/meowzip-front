8a9d1f557a0f72c83a8f0f8ce7c7c44054237c62
rectanglehihat <rectanglehihat@gmail.com>
2025-05-14 23:24:13 +0900

    Fix: useWebView 내부 알림 클릭시 함수 수정

--- src/hooks/useWebView.ts
+++ src/hooks/useWebView.ts
@@ -269,23 +269,23 @@ export const useWebView = (): UseWebViewReturn => {
               });
             }
             break;
-          case WEBVIEW_MESSAGE_TYPES.NOTIFICATION_CLICKED:
-            if (data.notification) {
-              console.log(
-                '[웹→앱] 알림 클릭 성공:',
-                JSON.stringify(data.notification)
-              );
-              localStorage.setItem(
-                'click_noti',
-                JSON.stringify(data.notification)
-              );
-              safePostMessage({
-                type: WEBVIEW_MESSAGE_TYPES.NOTIFICATION_CLICKED,
-                notification: data.notification,
-                timestamp: new Date().toISOString()
-              });
-            }
-            break;
+          // case WEBVIEW_MESSAGE_TYPES.NOTIFICATION_CLICKED:
+          //   if (data.notification) {
+          //     console.log(
+          //       '[웹→앱] 알림 클릭 성공:',
+          //       JSON.stringify(data.notification)
+          //     );
+          //     localStorage.setItem(
+          //       'click_noti',
+          //       JSON.stringify(data.notification)
+          //     );
+          //     safePostMessage({
+          //       type: WEBVIEW_MESSAGE_TYPES.NOTIFICATION_CLICKED,
+          //       notification: data.notification,
+          //       timestamp: new Date().toISOString()
+          //     });
+          //   }
+          //   break;
           case 'ready':
           case 'can-inline-scripts':
           case 'init-reply':
@@ -365,18 +365,18 @@ export const useWebView = (): UseWebViewReturn => {
       });
     }
 
-    const currentClickedNoti = localStorage.getItem('click_noti');
-    const parsedNotification = currentClickedNoti
-      ? JSON.parse(currentClickedNoti)
-      : null;
-    if (currentClickedNoti) {
-      console.log('[웹→앱] 저장된 알림 클릭:', parsedNotification);
-      safePostMessage({
-        type: WEBVIEW_MESSAGE_TYPES.NOTIFICATION_CLICKED,
-        notification: parsedNotification,
-        timestamp: new Date().toISOString()
-      });
-    }
+    // const currentClickedNoti = localStorage.getItem('click_noti');
+    // const parsedNotification = currentClickedNoti
+    //   ? JSON.parse(currentClickedNoti)
+    //   : null;
+    // if (currentClickedNoti) {
+    //   console.log('[웹→앱] 저장된 알림 클릭:', parsedNotification);
+    //   safePostMessage({
+    //     type: WEBVIEW_MESSAGE_TYPES.NOTIFICATION_CLICKED,
+    //     notification: parsedNotification,
+    //     timestamp: new Date().toISOString()
+    //   });
+    // }
 
     return () => {
       console.log('[웹→앱] 이벤트 리스너 제거');
