d0afa36ff07e12f86f238e580f89f0a11374aad4
rectanglehihat <rectanglehihat@gmail.com>
2025-05-16 21:38:18 +0900

    Test: useClickNoti 파일 상세 페이지에서 삭제

--- src/app/community/[slug]/page.tsx
+++ src/app/community/[slug]/page.tsx
@@ -16,7 +16,6 @@ import useFeedMutations from '@/hooks/community/useFeedMutations';
 import useCommentMutation from '@/hooks/community/useCommentMutation';
 import { readNotificationOnServer } from '@/services/profile';
 import { useWebView } from '@/hooks/useWebView';
-import { useClickNoti } from '@/hooks/common/useClickNoti';
 
 const DetailPage = ({ params: { slug } }: { params: { slug: number } }) => {
   const router = useRouter();
@@ -81,7 +80,6 @@ const DetailPage = ({ params: { slug } }: { params: { slug: number } }) => {
 
   // -------------------- test -------------------- //
   const { safePostMessage } = useWebView();
-  const { notification } = useClickNoti();
   const readNotification = useMutation({
     mutationFn: ({ id }: { id: number; type: string }) =>
       readNotificationOnServer(id),
@@ -93,18 +91,7 @@ const DetailPage = ({ params: { slug } }: { params: { slug: number } }) => {
       }
     }
   });
-  // useEffect(() => {
-  //   if (notification?.type !== 'COMMUNITY') return;
-  //   readNotification.mutate({
-  //     id: Number(notification['notification-id']),
-  //     type: notification.type
-  //   });
-  //   safePostMessage({
-  //     type: 'NOTIFICATION_CLICKED',
-  //     notification: notification,
-  //     timestamp: 123123
-  //   });
-  // }, [notification]);
+
   useEffect(() => {
     const storedClickNoti = localStorage.getItem('click_noti') || '';
     const parsedNotification = storedClickNoti
@@ -119,11 +106,6 @@ const DetailPage = ({ params: { slug } }: { params: { slug: number } }) => {
       type: 'NOTIFICATION_CLICKED',
       notification: parsedNotification,
       timestamp: 123123
-    });
-    safePostMessage({
-      type: 'NOTIFICATION_CLICKED',
-      notification: notification,
-      timestamp: 999999
     });
 
     return () => {
