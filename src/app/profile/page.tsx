f803245296f1d51ca16d4f437a31ba4639931689
rectanglehihat <rectanglehihat@gmail.com>
2025-06-12 22:59:22 +0900

    Style: 프로필 페이지 하단 여백 수정

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -186,7 +186,7 @@ export default function ProfilePage() {
             )
           )}
           {/* 무한 스크롤 감지 영역 */}
-          <div ref={postsRef} className="h-20 bg-gr-50" />
+          <div ref={postsRef} className="h-28 bg-gr-50" />
         </TabsContent>
         <TabsContent
           value="savedContents"
